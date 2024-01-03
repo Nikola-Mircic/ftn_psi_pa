c61b64721df870c0d0c73fde9ad37555d4931361
Nikola-Mircic <nikola123mircic@gmail.com>
2024-01-03 15:40:40 +0100

    Genetic algorithm module

--- genetic_algorithm/genetic_algorithm.jl
+++ genetic_algorithm/genetic_algorithm.jl
@@ -1,3 +1,5 @@
+module GeneticAlgorithm
+
 include("population.jl")
 include("crossover.jl")
 
@@ -46,3 +48,116 @@ function shouldStop(iter::Int, bestFitnes)
     return false
 end
 
+# ANALYZATION
+
+function getAverage(population::Vector{Entity}, elitePercent::Float64, mutationPercent::Float64, crossoverFunc!::Function, iter::Int)
+    avg_gen = 0
+    avg_best = 0
+
+    for i in 1:100
+        gen_i, best_i = geneticAlgorithm(population, 
+                                       elitePercent, 
+                                       mutationPercent, 
+                                       crossoverFunc!, 
+                                       numOfIterations)
+        avg_gen += gen_i
+        avg_best += best_i.fitness
+    end
+
+    return (avg_gen/100, avg_best/100)
+end
+
+function analyzeElitePercentage(values::Array{Float64})
+    results = zeros(length(values), 2)
+
+    for i in 1:length(values)
+        population_i = generatePopulation(populationSize, genesLength, minGene, maxGene)
+
+        gen_i, best_i = getAverage(population_i, 
+                           values[i], 
+                           mutationPercent, 
+                           makeCrossoverFunc([1;3]), 
+                           numOfIterations)
+        
+        results[i, 1] = gen_i
+        results[i, 2] = best_i
+    end
+
+    return results
+end
+
+function analyzeMutationPercentage(values::Array{Float64})
+    results = zeros(length(values), 2)
+
+    for i in 1:length(values)
+        population_i = generatePopulation(populationSize, genesLength, minGene, maxGene)
+
+        gen_i, best_i = getAverage(population_i, 
+                           elitePercent, 
+                           values[i], 
+                           makeCrossoverFunc([1;3]), 
+                           numOfIterations)
+        
+        results[i, 1] = gen_i
+        results[i, 2] = best_i
+    end
+
+    return results
+end
+
+function analyzePopulationSize(values::Vector{Int})
+    results = zeros(length(values), 2)
+
+    for i in 1:length(values)
+        population_i = generatePopulation(values[i], genesLength, minGene, maxGene)
+
+        gen_i, best_i = getAverage(population_i, 
+                                   elitePercent, 
+                                   mutationPercent, 
+                                   makeCrossoverFunc([1;3]), 
+                                   numOfIterations)
+        
+        results[i, 1] = gen_i
+        results[i, 2] = best_i
+    end
+
+    return results
+end
+
+function analyzeNumberOfIteration(values::Vector{Int})
+    results = zeros(length(values), 2)
+
+    for i in 1:length(values)
+        population_i = generatePopulation(populationSize, genesLength, minGene, maxGene)
+
+        gen_i, best_i = getAverage(population_i, 
+                                   elitePercent, 
+                                   mutationPercent, 
+                                   makeCrossoverFunc([1;3]), 
+                                   values[i])
+        
+        results[i, 1] = gen_i
+        results[i, 2] = best_i
+    end
+
+    return results
+end
+
+using Printf
+
+function printResult(values, results)
+    for i in eachindex(values)
+        @printf "%5.2f | %5.2f , %5.2f \n" values[i] results[i,1] results[i,2]
+    end
+end
+
+export geneticAlgorithm
+export getAverage
+export analyzeElitePercentage
+export analyzeMutationPercentage
+export analyzeNumberOfIteration
+export analyzePopulationSize
+
+export printResult
+
+end
